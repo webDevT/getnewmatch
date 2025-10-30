cda507516e8751d3efcff8ca7f5084d1cf6d470e
webDevT <karinadesyatnik27@gmail.com>
2025-10-30 12:49:35 +0200

    updates

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,19 @@
-$(function(){
+document.addEventListener('DOMContentLoaded', function () {
+  
+  const giftItems = document.querySelectorAll('.gift-item');
+  giftItems.forEach(item => {
+    item.addEventListener('click', function () {
+      giftItems.forEach(el => el.classList.remove('active'));
+      this.classList.add('active');
+    });
+  });
 
 
-
+  const coinsItems = document.querySelectorAll('.buy-coins__item');
+  coinsItems.forEach(item => {
+    item.addEventListener('click', function () {
+      coinsItems.forEach(el => el.classList.remove('active'));
+      this.classList.add('active');
+    });
+  });
 });
